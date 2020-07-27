74904b533d94e930e8d1d258dbd4909ce1b4e227
amritha-tk <65520729+amritha-tk@users.noreply.github.com>
2020-07-27 12:20:28 +0530

    [amritha]Refactor:Modified the testcases

--- src/test/java/com/generics/FindMaximumTest.java
+++ src/test/java/com/generics/FindMaximumTest.java
@@ -7,87 +7,66 @@ public class FindMaximumTest {
 
     @Test
     public void givenMaxIntegerNumber_AtPositionOne_ReturnsSameNumber() {
-        FindMaximum maximum = new FindMaximum(30, 20, 10);
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals(30,result);
+        Integer maxInteger=new FindMaximum<>(40,20,30,10).testMaximum();
+        Integer expectedMax=40;
+        Assert.assertEquals(expectedMax,maxInteger);
     }
 
     @Test
     public void givenMaxIntegerNumber_AtPositionTwo_ReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum(10, 30, 20);
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals(30,result);
+        Integer maxInteger=new FindMaximum<>(20,40,30,10).testMaximum();
+        Integer expectedMax=40;
+        Assert.assertEquals(expectedMax,maxInteger);
     }
 
     @Test
     public void givenMaxIntegerNumber_AtPositionThree_ReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum(10, 20, 30);
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals(30,result);
+        Integer maxInteger=new FindMaximum<>(30,20,40,10).testMaximum();
+        Integer expectedMax=40;
+        Assert.assertEquals(expectedMax,maxInteger);
     }
 
     @Test
     public void givenMaxFloatNumber_AtPositionOne_ReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum(30.56f, 20.54f, 10.57f);
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals(30.56f,result);
+        Float maxFloat=new FindMaximum<>(40.56f,20.54f,30.56f,10.57f).testMaximum();
+        Float expectedMax=40.56f;
+        Assert.assertEquals(expectedMax,maxFloat);
     }
 
     @Test
     public void givenMaxFloatNumber_AtPositionTwo_ReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum(20.54f, 30.56f, 10.57f);
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals(30.56f,result);
+        Float maxFloat=new FindMaximum<>(20.54f, 40.56f, 10.57f,30.56f).testMaximum();
+        Float expectedMax=40.56f;
+        Assert.assertEquals(expectedMax,maxFloat);
     }
 
     @Test
     public void givenMaxFloatNumber_AtPositionThree_ReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum(10.57f, 20.54f, 30.56f);
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals(30.56f,result);
+        Float maxFloat=new FindMaximum<>(20.54f, 10.57f, 40.56f,30.56f).testMaximum();
+        Float expectedMax=40.56f;
+        Assert.assertEquals(expectedMax,maxFloat);
     }
 
     @Test
     public void givenStringMax_AtPositionOne_ReturnSameString() {
-        FindMaximum maximum = new FindMaximum("Peach", "Apple", "Banana");
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals("Peach",result);
+        String maximum = new FindMaximum<>("Peach", "Apple", "Banana","Grapes").testMaximum();
+        String expectedMax ="Peach";
+        Assert.assertEquals(expectedMax,maximum);
     }
 
     @Test
     public void givenStringMax_AtPositionTwo_ReturnSameString() {
-        FindMaximum maximum = new FindMaximum("Apple", "Peach", "Banana");
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals("Peach",result);
+        String maximum = new FindMaximum<>("Apple", "Peach", "Banana","Grapes").testMaximum();
+        String expectedMax ="Peach";
+        Assert.assertEquals(expectedMax,maximum);
     }
 
     @Test
     public void givenStringMax_AtPositionThree_ReturnSameString() {
-        FindMaximum maximum = new FindMaximum("Apple", "Banana", "Peach");
-        Comparable result = maximum.testMaximum();
-        Assert.assertEquals("Peach",result);
+        String maximum = new FindMaximum<>("Banana", "Apple", "Peach","Grapes").testMaximum();
+        String expectedMax ="Peach";
+        Assert.assertEquals(expectedMax,maximum);
     }
-
-    @Test
-    public void givenMaxIntegerNumber_AtOptionalPosition_ReturnsSameNumber(){
-        FindMaximum maximum=new FindMaximum(10,20,30,40);
-        Comparable result=maximum.testMaximum();
-        Assert.assertEquals(40,result);
-    }
-
-    @Test
-    public void givenMaxFloatNumber_AtOptionalPosition_ReturnsSameNumber(){
-        FindMaximum maximum=new FindMaximum(30.56f,20.54f,10.57f,40.56f);
-        Comparable result=maximum.testMaximum();
-        Assert.assertEquals(40.56f,result);
-    }
-
-    @Test
-    public void givenStringMax_AtOptionalPosition_ReturnsSameString(){
-        FindMaximum maximum=new FindMaximum("Grapes","Apple","Banana","Peach");
-        Comparable result=maximum.testMaximum();
-        Assert.assertEquals("Peach",result);
-    }
-
 }
+
 
